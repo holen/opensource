97f3962651877169d6138f7ab2c70e2b2c5dc9a3
holen <yh_zhl@sina.com>
2014-02-25 22:22:14 +0800

    pull git shell

---
+++ git/pullgit.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd /data/git
+cd Network
+git pull https://github.com/holen/Network.git
+cd ../bash/
+git pull https://github.com/holen/bash.git
+cd ../opensource/
+git pull https://github.com/holen/opensource.git
+cd ../Python/
+git pull https://github.com/holen/Python.git
+
+cd /data/git
