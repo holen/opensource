c74965ec7e78d7b68823b7aab1f9bc22f4715719
holen <yh_zhl@sina.com>
2014-02-26 16:16:01 +0800

    pushgit for python

---
+++ git/pushgit.py
@@ -0,0 +1,48 @@
+#-* coding:UTF-8 -*                                                                                                 
+#!/usr/bin/env python
+import getopt, sys
+import subprocess
+import os
+
+def pushmygit(repo, commit_comment):
+    subprocess.check_call('bash /data/git/pullgit.sh', shell=True)
+    os.chdir('/data/git/%s' %(repo))
+    subprocess.check_call('git add .', shell=True)
+    subprocess.check_call('git commit -m "%s"' %(commit_comment), shell=True)
+    subprocess.check_call('git push -u origin master', shell=True)
+
+def usage():
+    '''Command help'''
+    print("Usage:%s [-h|-r|-c] [--help|--repo=""|--commit=""] args...." % sys.argv[0]);
+
+def parseargv():
+    '''Parse argv '''
+    try:                                                                                                            
+        opts, args = getopt.getopt(sys.argv[1:], "hr:c:", ["help", "repo=", "commit="])
+    except getopt.GetoptError as err:
+        print str(err) 
+        usage()
+        sys.exit(2)
+    for o, a in opts:
+        if o in ("-h", "--help"):
+            usage()
+            sys.exit()
+        elif o in ("-r", "--repo"):
+            repo = a
+        elif o in ("-c", "--commit"):
+            commit = a
+        else:
+            assert False, "unhandled option"
+    if repo and commit :
+        print repo, commit
+        return repo,commit
+    else:
+        print "Please get more option"
+        sys.exit(1)
+
+def main():
+    repo, commit_comment = parseargv()    
+    pushmygit(repo, commit_comment)
+
+if __name__ == "__main__" :
+    main()
